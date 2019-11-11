30c82f48d682a51157046c3ec7135685306152fa
tangjq-python <tangjq_wlkq@163.com>
2019-11-11 23:06:44 +0800

    同步

---
+++ firstapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def first(request):
+
+    return HttpResponse()
+
+def second(request):
+
+    return  HttpResponse()
